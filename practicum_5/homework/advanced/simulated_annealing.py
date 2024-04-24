cad1b6f4ff42dc2e6a6b766c7cf9809cdc629aa3
haphop228 <krivoshein.2014@list.ru>
2024-04-25 00:09:59 +0300

    [simulated_annealing] Some fixes, some attempts make algorithm better

--- practicum_5/homework/advanced/simulated_annealing.py
+++ practicum_5/homework/advanced/simulated_annealing.py
@@ -6,8 +6,6 @@ from src.plotting import plot_graph, plot_loss_history
 
 NDArrayInt = NDArray[np.int_]
 
-exponent = 2.718281828459045235360287471352
-
 def number_of_conflicts(G, colors):
     set_colors(G, colors)
     n = 0
@@ -21,12 +19,16 @@ def set_colors(G, colors):
     for n, color in zip(G.nodes, colors):
         G.nodes[n]["color"] = color
 
-def tweak(colors, n_max_colors):
+def tweak(colors, n_max_colors, G):
     new_colors = colors.copy()
     random_index = np.random.randint(0, len(colors))
     new_colors[random_index] = np.random.randint(0, n_max_colors - 1)
-    
+    new_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
     return new_colors
+
+def temperature_changing(temperature, i) -> int:
+    temperature = temperature - (((i + 1)))
+    return temperature
 
 def new_tweaks(colors, n_max_colors):
     new_colors = colors.copy()
@@ -48,33 +50,42 @@ def solve_via_simulated_annealing(
     #next_colors = initial_colors.copy()
     next_colors_best = initial_colors.copy()
 
-    temperature = 250
-    speed_of_cooling_down = 1
-
+    temperature = 2.5
+    speed_of_cooling_down = 0.01
+    arr_of_loss_history = []
 
     for i in range(n_iters):
         if(temperature != 0):
             loss_history[i] = number_of_conflicts(G, cur_colors)
-            next_colors_best = tweak(cur_colors, n_max_colors)
+            arr_of_loss_history.append(loss_history[i])
+            next_colors_best = tweak(cur_colors, n_max_colors, G)
             
             cur_confl = number_of_conflicts(G, cur_colors)
             new_confl = number_of_conflicts(G, next_colors_best)
             delta_energy = new_confl - cur_confl
             if (delta_energy <= 0):
                 cur_colors = next_colors_best
-                temperature -= speed_of_cooling_down
+                #temperature -= speed_of_cooling_down
+                temperature = temperature_changing(temperature=temperature, i=i)
                 if (temperature == 0):
-                    return loss_history
+                    print(min(arr_of_loss_history))
+                    break
+                    #return loss_history
             else:
-                probability = exponent * ((delta_energy * (-1)) / temperature)
-                num = np.random.randint(0, 1)
-                temperature -= speed_of_cooling_down
+                probability = 1
+                #np.exp((delta_energy * (-1)) / temperature)
+                num = np.random.uniform(0, 1)
+                #print(num)
+                #temperature -= speed_of_cooling_down
+                temperature = temperature_changing(temperature=temperature, i=i)
                 if (num <= probability):
                     cur_colors = next_colors_best
                 if (temperature == 0):
-                    return loss_history
-        else:
-            break
+                    print(min(arr_of_loss_history))
+                    break
+                    #return loss_history
+        print(min(arr_of_loss_history))
+                 
     return loss_history
     
 
@@ -83,7 +94,7 @@ if __name__ == "__main__":
     np.random.seed(seed)
     G = nx.erdos_renyi_graph(n=100, p=0.05, seed=seed)
     #plot_graph(G)
-
+    
     n_max_iters = 250
     n_max_colors = 3
     initial_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
