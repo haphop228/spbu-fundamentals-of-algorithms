85df569e62dcb8927a22c8c728eacd92d7812a0b
haphop228 <krivoshein.2014@list.ru>
2024-04-25 20:54:10 +0300

    [simulated_annealing] Now record is 49

--- practicum_5/homework/advanced/simulated_annealing.py
+++ practicum_5/homework/advanced/simulated_annealing.py
@@ -14,31 +14,54 @@ def number_of_conflicts(G, colors):
             n += 1
     return n
 
-
 def set_colors(G, colors):
     for n, color in zip(G.nodes, colors):
         G.nodes[n]["color"] = color
 
 def tweak(colors, n_max_colors, G):
     new_colors = colors.copy()
-    random_index = np.random.randint(0, len(colors))
+    #arr_random_index = []
+    for j in range(len(colors) // 20):
+        random_index = np.random.randint(low=0, high=len(colors))
+        #arr_random_index.append(random_index)
+        #new_colors[random_index] = np.random.randint(0, n_max_colors - 1)
     new_colors[random_index] = np.random.randint(0, n_max_colors - 1)
-    new_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
+    #new_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
     return new_colors
 
 def temperature_changing(temperature, i) -> int:
-    temperature = temperature - (((i + 1)))
+    lamda = 0.99 
+    #* int((i // 5) + 1) 
+    #iter = 500 - 2 * i
+    temperature = temperature * lamda
+    #temperature = temperature ** np.log(temperature) * lamda
+    #temperature = temperature * np.cos(temperature) 
+    #temperature = temperature ** np.log(temperature) * lamda
+    #np.exp(temperature) 
     return temperature
 
 def new_tweaks(colors, n_max_colors):
     new_colors = colors.copy()
-    rand_num1 = np.random.randint(low=0, high=len(colors))
-    rand_num2 = np.random.randint(low=0, high=len(colors))
-    while (rand_num1 == rand_num2): # If num1 == num2 create new one until they different
-        rand_num2 = np.random.randint(low=0, high=len(colors))
+    for j in range(len(colors) // 30):
+        random_index1 = np.random.randint(low=0, high=len(colors))
+        random_index2 = np.random.randint(low=0, high=len(colors))
+        #arr_random_index.append(random_index)
+        #new_colors[random_index1] = np.random.randint(0, n_max_colors - 1)
+        new_colors[random_index1] = new_colors[random_index2]
     
-    if (rand_num1 > rand_num2):
-        new_colors = new_colors[rand_num2::] + new_colors[rand_num2:rand_num1:-1] + new_colors[rand_num1::]
+    #new_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
+    return new_colors
+
+def new_new_tweaks(colors, n_max_colors):
+    new_colors = colors.copy()
+    rand_color = np.random.randint(0, n_max_colors - 1)
+    random_index1 = np.random.randint(low=0, high=len(colors))
+    if (random_index1 + 1) in new_colors:
+        while (new_colors[random_index1] != new_colors[random_index1 + 1] and random_index1 < len(colors) - 1):
+            new_colors[random_index1] = rand_color
+            random_index1 +=1
+        
+    return new_colors
 
 
 def solve_via_simulated_annealing(
@@ -49,45 +72,50 @@ def solve_via_simulated_annealing(
     cur_colors = initial_colors
     #next_colors = initial_colors.copy()
     next_colors_best = initial_colors.copy()
-
-    temperature = 2.5
-    speed_of_cooling_down = 0.01
+    # 50 - loss = 66
+    # 4 - loss = 61
+    # 2 - loss = 59
+    # 1 - loss = 56
+    # new_tweaks 1 - loss = 49 //30
+    counter = 0
+    temperature = 1
+    speed_of_cooling_down = 1
     arr_of_loss_history = []
-
+    probability_history = np.zeros((n_iters,), dtype=np.float64)
+    temperature_history = np.zeros((n_iters,), dtype=np.float64)
     for i in range(n_iters):
         if(temperature != 0):
             loss_history[i] = number_of_conflicts(G, cur_colors)
             arr_of_loss_history.append(loss_history[i])
-            next_colors_best = tweak(cur_colors, n_max_colors, G)
+            next_colors_best = new_new_tweaks(cur_colors, n_max_colors)
+            #next_colors_best = tweaks(cur_colors, n_max_colors, G)
             
             cur_confl = number_of_conflicts(G, cur_colors)
             new_confl = number_of_conflicts(G, next_colors_best)
             delta_energy = new_confl - cur_confl
             if (delta_energy <= 0):
                 cur_colors = next_colors_best
-                #temperature -= speed_of_cooling_down
                 temperature = temperature_changing(temperature=temperature, i=i)
                 if (temperature == 0):
-                    print(min(arr_of_loss_history))
+                    #print(min(arr_of_loss_history))
                     break
-                    #return loss_history
             else:
-                probability = 1
-                #np.exp((delta_energy * (-1)) / temperature)
+                probability = np.exp((delta_energy * (-1)) / temperature)
+                probability_history[counter] = probability
                 num = np.random.uniform(0, 1)
-                #print(num)
-                #temperature -= speed_of_cooling_down
                 temperature = temperature_changing(temperature=temperature, i=i)
                 if (num <= probability):
                     cur_colors = next_colors_best
                 if (temperature == 0):
-                    print(min(arr_of_loss_history))
+                    #print(min(arr_of_loss_history))
                     break
-                    #return loss_history
-        print(min(arr_of_loss_history))
-                 
+            temperature_history[counter] = temperature
+            counter+=1
+        
+    print(min(arr_of_loss_history))
+    plot_loss_history(probability_history)
+    #plot_loss_history(temperature_history) 
     return loss_history
-    
 
 if __name__ == "__main__":
     seed = 42
@@ -95,11 +123,11 @@ if __name__ == "__main__":
     G = nx.erdos_renyi_graph(n=100, p=0.05, seed=seed)
     #plot_graph(G)
     
-    n_max_iters = 250
+    n_max_iters = 500
     n_max_colors = 3
     initial_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
 
     loss_history = solve_via_simulated_annealing(
         G, n_max_colors, initial_colors, n_max_iters
     )
-    plot_loss_history(loss_history)
+    #plot_loss_history(loss_history)
